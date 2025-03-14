6a04ddab1dc64454b87e8a767ccc7fc2b319a498
diptendu07 <diptendulodh92@gmail.com>
2025-03-14 23:58:22 +0530

    Added homepage message with API links

--- top10-reviews-backend/src/app.js
+++ top10-reviews-backend/src/app.js
@@ -12,6 +12,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+    res.send(`
+      <h1>Welcome to the Movie & Game Review API</h1>
+      <p>Use the following endpoints:</p>
+      <ul>
+        <li><a href="/api/movies">/api/movies</a> - Get top 10 movies</li>
+        <li><a href="/api/games">/api/games</a> - Get top 10 games</li>
+      </ul>
+    `);
+  });
+
 // Routes
 app.use("/api/movies", require("./routes/movieRoutes"));
 app.use("/api/games", require("./routes/gameRoutes"));
